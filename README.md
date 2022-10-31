41a94238e08c427e7a1ca14496655eb42e4e9f04
DBR-3 <dimabychkov01@gmail.com>
2022-10-31 17:19:02 +0300

    eng version 0.0

--- README.md
+++ README.md
@@ -1,20 +1,62 @@
-# Time-series 
-Designed for forecasting time series based on period data
+<h1 align="center"> Time Series </h1>
+<h3 align="center"> Designed for forecasting time series based on period data </h3>
+
+<h2 align="center"> Description </h2>
+
+<h4 align="center"> General classification of forecasting methods </h4>
+
+```mermaid
+graph TD;
+    Forecasting_Methods-->Subjective_Forecasting;
+    Forecasting_Methods-->Objective_Forecasting;
+    Objective_Forecasting-->Domain_Models;
+    Objective_Forecasting-->Time_Series_Methods;
+    Time_Series_Methods-->Statistical_Model;
+    Time_Series_Methods-->Structural_Models;
+    Statistical_Model--->Regression;
+    Statistical_Model-->Autoregression;
+    Statistical_Model--->Exponential_Smoothing;
+    Structural_Models-->Neural_Networks;
+    Structural_Models--->Decision_Tree_Learning;
+    Structural_Models-->Markov_Chain;
+```
+<h4 align="center"> Since we are forecasting time series, we choose statistical models such as Holt's linear trend, Holt-Winter method and SARIMA </h4>
 
 
-## Functionality
+<h4 align="left">SARIMA</h4>
+<ul>
+  <li>S-Seasonal</li>
+  <li>AR-Auto Regression</li>
+  <li>I-Integration</li>
+  <li>MA-Moving Average</li>
+</ul>
 
-### Description
-
-
-
+<h2 align="center"> Functionality </h2>
 
 ```mermaid
 graph TD;
     Node_Exporter-->Prometheus;
+    Prometheus-->Node_Exporter;
     Prometheus-->Storge;
     Storge-->Web-server;
     Web-server-->Grafana;
 ```
 
-##### Libraries
+
+
+<h3 align="left"> Libraries </h3>
+
+NumPy to do math
+https://numpy.org/
+
+Pandas for working with data
+https://pandas.pydata.org/docs/index.html
+
+Matplotlib to plot the graph
+https://matplotlib.org/
+
+statsmodels for Holt-Winter and SARIMA calculations
+https://www.statsmodels.org/stable/index.html
+
+sklearn and math to calculate the mean square error
+https://scikit-learn.org/stable/ and https://docs.python.org/3/library/math.html
--- time_series.py
+++ time_series.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.tsa.api import ExponentialSmoothing, Holt
@@ -10,11 +10,8 @@ df = pd.read_csv('/Users/dimabyckov/Desktop/Работа/DataSets/Holt-ARIMA', s
 df.head()
 df.tail()
 df = pd.read_csv('/Users/dimabyckov/Desktop/Работа/DataSets/Holt-ARIMA', sep=",")
-Start_Train = int(input())
-End_Train = int(input())
-train = df[Start_Train:End_Train]
-test = df[End_Train:]
-
+train = df[0:10392]
+test = df[10392:]
 
 
 train.Count.plot(figsize=(16, 8), title='Daily Ridership', fontsize=14)
@@ -29,11 +26,11 @@ y_hat_avg = test.copy()
 fit1 = Holt(np.asarray(train['Count'])).fit(smoothing_level=0.3, smoothing_slope=0.1)
 y_hat_avg['Holt_linear'] = fit1.forecast(len(test))
 plt.figure(figsize=(16, 8))
-plt.plot(train['Count'], label='Тренировка')
-plt.plot(test['Count'], label='Тест')
-plt.plot(y_hat_avg['Holt_linear'], label='Линейный тренд Холта')
+plt.plot(train['Count'], label='Train')
+plt.plot(test['Count'], label='Test')
+plt.plot(y_hat_avg['Holt_linear'], label="Holt's linear trend")
 plt.legend(loc='best')
-plt.title("Линейный тренд Холта")
+plt.title("Holt's linear trend")
 plt.show()
 
 
@@ -41,11 +38,11 @@ y_hat_avg = test.copy()
 fit1 = ExponentialSmoothing(np.asarray(train['Count']), seasonal_periods=7, trend='add', seasonal='add', ).fit()
 y_hat_avg['Holt_Winter'] = fit1.forecast(len(test))
 plt.figure(figsize=(16, 8))
-plt.plot(train['Count'], label='Тренировка')
-plt.plot(test['Count'], label='Тест')
-plt.plot(y_hat_avg['Holt_Winter'], label='Метод Холта-Винтера')
+plt.plot(train['Count'], label='Train')
+plt.plot(test['Count'], label='Test')
+plt.plot(y_hat_avg['Holt_Winter'], label='Holt-Winter method')
 plt.legend(loc='best')
-plt.title("Метод Холта-Винтера")
+plt.title("Holt-Winter method")
 plt.show()
 
 rms = sqrt(mean_squared_error(test.Count, y_hat_avg.Holt_Winter))
@@ -56,8 +53,8 @@ y_hat_avg = test.copy()
 fit1 = sm.tsa.statespace.SARIMAX(train.Count, order=(7, 1, 4), seasonal_order=(0, 1, 2, 7)).fit()
 y_hat_avg['SARIMA'] = fit1.predict(start="2013-11-1", end="2013-12-31", damped=True)
 plt.figure(figsize=(16, 8))
-plt.plot(train['Count'], label='Тренировка')
-plt.plot(test['Count'], label='Тест')
+plt.plot(train['Count'], label='Train')
+plt.plot(test['Count'], label='Test')
 plt.plot(y_hat_avg['SARIMA'], label='SARIMA')
 plt.legend(loc='best')
 plt.title("SARIMA")
